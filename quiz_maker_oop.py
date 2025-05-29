18db1d0cf5c928e572ecb6217a53717770bf72fd
allenandreim <allenandrei8209@gmail.com>
2025-05-30 02:50:03 +0800

    make_class_to_keep_related_data_together

---
+++ quiz_maker_oop.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class QuizMakerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Quiz Maker")
+        self.root.geometry("400x450")
